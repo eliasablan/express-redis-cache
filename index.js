6e74aebb0c1118bc2c1e8b670c62bb5d4a15c7e4
Elias Ablan <eliasablan93@gmail.com>
2025-02-20 14:10:59 -0400

    simplest example of express server

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import axios from "axios";
+import { createClient } from "redis";
+
+const app = express();
+
+const client = createClient({
+  host: "localhost",
+  port: 6379,
+});
+
+app.get("/characters", async (req, res) => {
+  const reply = await client.get("characters");
+
+  if (reply) return res.json(JSON.parse(reply));
+
+  const { data } = await axios.get("https://rickandmortyapi.com/api/character");
+
+  const savedResult = await client.set("characters", JSON.stringify(data));
+  console.log(savedResult);
+
+  return res.json(data);
+});
+
+async function main() {
+  client.connect();
+  app.listen(3000);
+  console.log("Server is running on port 3000");
+}
+
+main();
